41571bc3c6d595a1580e80b6ecd88ecee3d0f662
sunyanwu <sunyanwu@aixcoder.com>
2025-12-02 14:29:08 +0800

    格式化 ArticleMapper.java 文件的缩进

--- blogserver/src/main/java/org/sang/mapper/ArticleMapper.java
+++ blogserver/src/main/java/org/sang/mapper/ArticleMapper.java
@@ -11,34 +11,34 @@ import java.util.List;
  */
 @Mapper
 public interface ArticleMapper {
-    int addNewArticle(Article article);
+        int addNewArticle(Article article);
 
-    int updateArticle(Article article);
+        int updateArticle(Article article);
 
-    List<Article> getArticleByState(@Param("state") Integer state, @Param("start") Integer start,
-            @Param("count") Integer count, @Param("uid") Long uid, @Param("keywords") String keywords);
+        List<Article> getArticleByState(@Param("state") Integer state, @Param("start") Integer start,
+                        @Param("count") Integer count, @Param("uid") Long uid, @Param("keywords") String keywords);
 
-    // List<Article> getArticleByStateByAdmin(@Param("start") int start,
-    // @Param("count") Integer count, @Param("keywords") String keywords);
+        // List<Article> getArticleByStateByAdmin(@Param("start") int start,
+        // @Param("count") Integer count, @Param("keywords") String keywords);
 
-    int getArticleCountByState(@Param("state") Integer state, @Param("uid") Long uid,
-            @Param("keywords") String keywords);
+        int getArticleCountByState(@Param("state") Integer state, @Param("uid") Long uid,
+                        @Param("keywords") String keywords);
 
-    int updateArticleState(@Param("aids") Long aids[], @Param("state") Integer state);
+        int updateArticleState(@Param("aids") Long aids[], @Param("state") Integer state);
 
-    int updateArticleStateById(@Param("articleId") Integer articleId, @Param("state") Integer state);
+        int updateArticleStateById(@Param("articleId") Integer articleId, @Param("state") Integer state);
 
-    int deleteArticleById(@Param("aids") Long[] aids);
+        int deleteArticleById(@Param("aids") Long[] aids);
 
-    Article getArticleById(Long aid);
+        Article getArticleById(Long aid);
 
-    void pvIncrement(Long aid);
+        void pvIncrement(Long aid);
 
-    // INSERT INTO pv(countDate,pv,uid) SELECT NOW(),SUM(pageView),uid FROM article
-    // GROUP BY uid
-    void pvStatisticsPerDay();
+        // INSERT INTO pv(countDate,pv,uid) SELECT NOW(),SUM(pageView),uid FROM article
+        // GROUP BY uid
+        void pvStatisticsPerDay();
 
-    List<String> getCategories(Long uid);
+        List<String> getCategories(Long uid);
 
-    List<Integer> getDataStatistics(Long uid);
+        List<Integer> getDataStatistics(Long uid);
 }
